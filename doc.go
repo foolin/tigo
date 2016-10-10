17f0d6a68e43e41fb8569514d358fd4c03315025
Foolin <foolin@126.com>
2016-10-10 11:12:12 +0800

    doc

--- doc.go
+++ doc.go
@@ -1,5 +1,4 @@
 /*
 Package tigo is forked from Fasthttp-routing which is a lightweight high performance HTTP request router, fast and powerful routing features for the high-performance.
 */
-
 package tigo
