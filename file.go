bf18bf462d3c37c313f5f17a112d62b1de27b399
Foolin <foolin@126.com>
2016-12-29 11:28:49 +0800

    server file

--- file.go
+++ file.go
@@ -2,13 +2,22 @@ package tigo
 
 import (
 	"github.com/valyala/fasthttp"
+	"log"
 )
 
 func Static(root string, stripSlashes int) Handler {
 	fsHandler := fasthttp.FSHandler(root, stripSlashes)
 	return func(ctx *Context) error {
-		//log.Printf("static file: %s", ctx.Request.URI().Path())
+		log.Printf("static file: %s", ctx.Request.URI().Path())
 		fsHandler(ctx.RequestCtx)
+		return nil
+	}
+}
+
+
+func File(path string) Handler {
+	return func(ctx *Context) error {
+		fasthttp.ServeFile(ctx.RequestCtx, path)
 		return nil
 	}
 }
